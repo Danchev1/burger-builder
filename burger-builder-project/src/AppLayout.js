c5bb31ef484f263c339aed0294db4d71b1894052
yordandanchev <yordangdanchev@gmail.com>
2019-01-13 21:11:05 +0200

    Burger Build project - removing layout component, using app component for that matter

--- burger-builder-project/src/AppLayout.js
+++ burger-builder-project/src/AppLayout.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './assets/sass/global/main.sass';
 import Header from "./Components/LayoutComponents/Header/Header";
 import Footer from "./Components/LayoutComponents/Footer/Footer";
-import MainLayout from "./Components/LayoutComponents/MainLayout/MainLayout";
 import BurgerBuilder from "./Containers/BurgerBuilder";
 
-const App = () => {
+const AppLayout = () => {
     return (
         <>
           <Header/>
-          <MainLayout>
+          <div>SideDrawerComponent, CartComponent, BackDrop(Квото и да е тва?!)</div>
+          <main className='MainLayout'>
             <BurgerBuilder/>
-          </MainLayout>
+          </main>
           <Footer/>
         </>
     )
 };
 
-export default App;
+export default AppLayout;
